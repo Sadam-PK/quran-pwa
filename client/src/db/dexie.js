8d7457386711680fd477f7d3998cc082a507cc45
sadam <sadamimac@sadams-iMac.local>
2024-12-06 03:04:55 +0500

    updated

--- client/src/db/dexie.js
+++ client/src/db/dexie.js
@@ -1,10 +1,10 @@
 // frontend/src/db/dexie.js
 import Dexie from "dexie";
 
-// Defines Dexie database
+// Define your Dexie database
 const db = new Dexie("MyDatabase");
 db.version(1).stores({
-  surahs: "++id, id, name, meaning, ayahs" // schema
+  surahs: "++id, id, name, meaning, ayahs" // Updated schema
   // Add more tables as needed
 });
 
